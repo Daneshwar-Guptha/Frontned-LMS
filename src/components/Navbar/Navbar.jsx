41385f296d197005f1629f23171cecf3eb8453f7
Daneshwar <saiguptha663@gmail.com>
2025-12-01 21:23:17 +0530

    changes on frontend

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,12 +11,15 @@ const Navbar = () => {
   const profile = ()=>{
     navigate("/Profile");
   }
+  const dashboard = ()=>{
+    navigate("/dashboard")
+  }
   return (
     <nav className="navbar">
      <img className="logo-img" src= "https://cdn-icons-png.flaticon.com/512/16847/16847436.png"/>
 
       <ul className="nav-links">
-        <li>Dashboard</li>
+        <li onClick={dashboard}>Dashboard</li>
         <li>My Courses</li>
         <li>Live Classes</li>
         <li>Assignments</li>
